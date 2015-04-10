a6a59e759d3d2d50fbc434caa3a4858dae95edb0
g12951te <g12951te@s30438.tsuda.ac.jp>
2015-04-10 11:53:34 +0900

    "Have a nice day!"の出力を追加しました

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -16,6 +16,7 @@ public class Helloworld {
     public static void main(String[] args) {
         System.out.println("こんにちは世界");
         System.out.println("HelloWorld");
+        System.out.println("Have a nice day!");
         // TODO code application logic here
     }
 }
